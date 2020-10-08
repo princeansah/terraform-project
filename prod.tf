6d5cda98e5de00879ef2d5a887f79f6d96984643
princeansah <prince.owusuansah@icloud.com>
2020-10-07 21:40:19 -0500

    split data into tfvars

--- prod.tf
+++ prod.tf
@@ -2,6 +2,33 @@
 # run "terraform init, then terraform validate to see if the configuration files are correct"
 # webgraphviz.com
 
+
+#variable definition block
+variable "region" {
+    type = string
+}
+variable "profile"  {
+    type = string
+}
+variable "ip_range" {
+    type = list(string)
+}              
+variable "web_image_id" {
+    type = string
+}           
+variable "web_instance_type"  {
+    type = string
+}     
+variable "web_desired_capacity"  {
+    type = number
+}           
+variable "web_max_size" {
+    type = number
+}           
+variable "web_min_size"  {
+    type = number
+}          
+
 # s3 bucket
 resource "aws_s3_bucket" "prod_tf_course" {
     bucket = "terraformproject23"  # to save your plan in a separate file, "terraform plan -'destroy' -out=name_of_file" to create an out file.
@@ -31,23 +58,23 @@ resource "aws_security_group" "prod_web" {
     name        = "prod_web"
     description = "Allow standard http and https ports inbound and everything outbound"
 
-    ingress {              # this is 
+    ingress {               
         from_port   = 80   # this lets you define an inbound port range
         to_port     = 80   # however because we have same port number and not a range we maintain same numbers
         protocol    = "tcp"
-        cidr_blocks = ["98.156.133.14/32"]
+        cidr_blocks = var.ip_range
     }
     ingress {
         from_port    = 443
         to_port      = 443
         protocol     = "tcp"
-        cidr_blocks  = ["98.156.133.14/32"]
+        cidr_blocks  = var.ip_range
     }
     egress {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"           # allowing traffic from anywhere
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.ip_range
     }
     tags = {
         "Terraform"   : "true"   # this provides a handy way to tell which resources are managed by terraform when you log in to 
@@ -57,15 +84,15 @@ resource "aws_security_group" "prod_web" {
 # auto scaling group
 resource "aws_launch_template" "prod_web" {     # this is just the configuration that your ASG should use to lunch new instances
     name_prefix   = "prod-web"
-    image_id      = "ami-019c091d13a1fa156"
-    instance_type = "t2.nano"
+    image_id      = var.web_image_id 
+    instance_type = var.web_instance_type
 }
 
 resource "aws_autoscaling_group" "prod_web" {
   vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]  #available and used subnets
-  desired_capacity    = 2             
-  max_size            = 2
-  min_size            = 1
+  desired_capacity    = var.web_desired_capacity              
+  max_size            = var.web_max_size 
+  min_size            = var.web_min_size
 
   launch_template {
     id      = aws_launch_template.prod_web.id
@@ -99,3 +126,10 @@ resource "aws_elb" "prod_web" {
         "Terraform" : "true"
     }    
 } 
+
+               
+           
+       
+             
+           
+            
---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+region                  = "us-west-2"
+profile                 = "default"
+ip_range               = ["0.0.0.0/0"]
+web_image_id            = "ami-019c091d13a1fa156"
+web_instance_type       = "t2.nano"
+web_desired_capacity    = 1           
+web_max_size            = 1
+web_min_size            = 1
